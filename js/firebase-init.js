4b7d72893f397804d9e221789151b73f1c88ce83
DIVYANAND A P <divyanand942@gmail.com>
2025-12-18 14:53:52 +0530

    Update firebase-init.js

--- js/firebase-init.js
+++ js/firebase-init.js
@@ -1,8 +1,3 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyCfTdbFDiyzeiRFW8_82Q2giK8Kb0BgjvM", // Use your actual key
-  authDomain: "noq-eats.firebaseapp.com",
-  projectId: "noq-eats",
-  storageBucket: "noq-eats.firebasestorage.app",
-  messagingSenderId: "691964454252",
-  appId: "1:691964454252:web:e5fdd05ad5145343573f49"
+  
 };
